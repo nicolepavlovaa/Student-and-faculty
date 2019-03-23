713e6e222168644b18fa14cd2bff433794cd0bdd
nicolepavlovaa <niki.0199@gmail.com>
2019-03-23 17:26:56 +0200

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Student.h"
+#include "Faculty.h"
+using namespace std;
+#include <cstring>
+
+int main()
+{
+	Student maya;
+	Student nicole;
+	Student anna;
+	Faculty FMI;
+	
+	maya.setName("Maya");
+	maya.setFn(10101);
+	maya.setCourse(2);
+	nicole.setName("Nicole");
+	nicole.setCourse(1);
+	nicole.setFn(20202);
+	cout << maya << endl;
+	cout << nicole << endl;
+	FMI += nicole;
+	if (maya == nicole)
+	{
+		cout << "yes" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
